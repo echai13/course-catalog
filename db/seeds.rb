08755f9a0d281991353641a5dd71ec3a9f04a57b
echai13 <ericachai@Ericas-MacBook-Air.local>
2017-03-21 15:39:37 -0400

    Add back JSON

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,29 +6,29 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# json = JSON.parse(File.read('db/instructor.json'))
-#
-# json.each do |a|
-#   Instructor.create(:lastname => a["last"], :firstname => a["first"], :email => a["email"])
-# end
-#
-#
-# json = JSON.parse(File.read('db/course.json'))
-#
-# json.each do |a|
-#   Course.create(:name => a["name"], :description => a["description"], :coursecode => a["code"], :subjectid => a["subjects"][0]["id"])
-# end
-#
-#
-# json = JSON.parse(File.read('db/subject.json'))
-#
-# json.each do |a|
-#   Subject.create(:name => a["name"], :subjectid => a["id"])
-# end
+json = JSON.parse(File.read('db/instructor.json'))
 
-# Course.all.each do |c|
-#   CourseSubject.create!(:course_id => c.id, :subject_id => Subject.all.find_by_subjectid(c.subjectid))
-# end
+json.each do |a|
+  Instructor.create(:lastname => a["last"], :firstname => a["first"], :email => a["email"])
+end
+
+
+json = JSON.parse(File.read('db/course.json'))
+
+json.each do |a|
+  Course.create(:name => a["name"], :description => a["description"], :coursecode => a["code"], :subjectid => a["subjects"][0]["id"])
+end
+
+
+json = JSON.parse(File.read('db/subject.json'))
+
+json.each do |a|
+  Subject.create(:name => a["name"], :subjectid => a["id"])
+end
+
+Course.all.each do |c|
+  CourseSubject.create!(:course_id => c.id, :subject_id => Subject.all.find_by_subjectid(c.subjectid))
+end
 
 
 Course.all.each do |c|
