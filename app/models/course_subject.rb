00cff4cdee2772262e2f04a49b49bf9a20a194f0
echai13 <ericachai@Ericas-MacBook-Air.local>
2017-03-21 16:13:33 -0400

    ILIKE-POSTgreSQL

--- app/models/course_subject.rb
+++ app/models/course_subject.rb
@@ -4,11 +4,11 @@ class CourseSubject < ApplicationRecord
 
   def self.search(search1, search2)
     if search2.blank?
-      CourseSubject.where("coursename LIKE ?", "%#{search1}%")
+      CourseSubject.where("coursename ILIKE ?", "%#{search1}%")
     elsif search1.blank?
-      CourseSubject.where("subject_id LIKE ?", "%#{search2}%")
+      CourseSubject.where("subject_id ILIKE ?", "%#{search2}%")
     else
-      CourseSubject.where("coursename LIKE ? AND subject_id LIKE ?", "%#{search1}%", "%#{search2}%")
+      CourseSubject.where("coursename ILIKE ? AND subject_id ILIKE ?", "%#{search1}%", "%#{search2}%")
     end
   end
 
