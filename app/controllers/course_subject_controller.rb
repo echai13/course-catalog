aa046cfca35cb3ed57becf8e8c35d7fc794bc776
echai13 <cher13@brandeis.edu>
2017-03-27 19:23:32 -0400

    Remove stupid ordering

--- app/controllers/course_subject_controller.rb
+++ app/controllers/course_subject_controller.rb
@@ -2,7 +2,7 @@ class CourseSubjectController < ApplicationController
 
   def index
     if params[:search] or params[:search]
-      @coursesubject = CourseSubject.search(params[:search], params[:search2]).order("created_at DESC").paginate(:per_page => 10, :page => params[:page])
+      @coursesubject = CourseSubject.search(params[:search], params[:search2]).paginate(:per_page => 10, :page => params[:page])
     else
       @coursesubject = CourseSubject.all.paginate(:per_page => 10, :page => params[:page])
     end
